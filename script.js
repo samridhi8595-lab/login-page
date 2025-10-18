718a820126115751835ae6f4f643b955ee600468
samridhi8595-lab <samridhi8595@gmail.com>
2025-10-18 15:07:17 +0530

    on click sign up go to next page

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const eyeicon = document.getElementById("eyeicon");
 const password = document.getElementById("password");
 const login = document.getElementById("login");
-const signup = document.getElementById("sign-up"); 
+
 
 eyeicon.onclick = function(){
     if(password.type == "password"){
@@ -16,6 +16,5 @@ eyeicon.onclick = function(){
 login.onclick = function(){
     alert("Successfully login");
 }
-signup.onclick = function(){
-    alert("successfully Sign up");
-}
+
+ 
